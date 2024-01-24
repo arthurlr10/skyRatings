cfccc88a2e35f3bc8858b863304d1e619192a1f6
Pierre <pierre.yvenou@orange.fr>
2024-01-25 00:32:25 +0100

    Adding logo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,27 @@
 import { RootStoreProvider } from "@mobx";
 import "@styles/global.css";
 import { AppProps } from "next/app";
+import Head from "next/head";
+import React from "react";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     return (
-        <RootStoreProvider>
-            <Component {...pageProps} />
-        </RootStoreProvider>
+        <div>
+            <Head>
+                <link
+                    rel="Website Icon"
+                    href="/icons/favicon.ico"
+                    type="image/x-icon"
+                />
+            </Head>
+            <Head>
+                <title>SkyRatings</title>
+            </Head>
+            <RootStoreProvider>
+                <Component {...pageProps} />
+            </RootStoreProvider>
+        </div>
+
     );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import GlobalContainer from "@components/globalContainer";
 import Hero from "@components/hero";
-
+import Head from "next/head";
 const Home: React.FC = () => {
     return (
         <GlobalContainer>
