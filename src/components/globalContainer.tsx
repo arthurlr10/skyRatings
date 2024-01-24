0c31494a44855253e6abb6ad07267fcccb2ba63c
Pierre <pierre.yvenou@orange.fr>
2024-01-24 17:30:18 +0100

    [Header] Refacto style

--- src/components/globalContainer.tsx
+++ src/components/globalContainer.tsx
@@ -5,7 +5,6 @@ import { useRouter } from "next/router";
 const { Header, Content, Footer, Sider } = Layout;
 const { Text } = Typography;
 
-
 const customTheme = {
     token: {
         colorPrimary: "#7EC2F9",
@@ -13,12 +12,12 @@ const customTheme = {
     },
     components: {
         Menu: {
-            activeBarHeight: 0
+            activeBarHeight: 0,
         },
         Layout: {
             headerBg: "transparent",
         },
-      },
+    },
 };
 
 const items = [
@@ -26,29 +25,67 @@ const items = [
     { key: "companies", label: "Companies" },
     { key: "airports", label: "Airports" },
     { key: "about", label: "About" },
-]
+];
 
 const GlobalContainer: React.FC = ({ children }) => {
     const router = useRouter();
 
     const handleMenuClick = (e: any) => {
-        router.push(`/${e.key}`);
+        console.log(e);
+        //router.push(`/${e.key}`);
     };
 
     return (
         <ConfigProvider theme={customTheme}>
             <Layout>
-                <Header style={{ display: "flex" }}>
-                    <Text style={{ color: "#7EC2F9", fontSize: "36px", flexShrink: 0 }}>SkyRatings</Text>
+                <Header
+                    style={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "flex-start",
+                    }}
+                >
+                    <Text
+                        style={{
+                            color: "#7EC2F9",
+                            fontSize: "36px",
+                            fontWeight: "600",
+                            flexShrink: 0,
+                        }}
+                    >
+                        SkyRatings
+                    </Text>
                     <Menu
                         mode="horizontal"
-                        defaultSelectedKeys={["flights"]}
-                        items={items}
                         onClick={handleMenuClick}
-                        style={{ width: "100%", backgroundColor: "transparent", borderBottom: "none" }}
-                    />
+                        style={{
+                            width: "80%",
+                            justifySelf: "center",
+                            display: "flex",
+                            alignItems: "center",
+                            flex: 1,
+                            minWidth: 0,
+                            justifyContent: "center",
+                            fontSize: "18px",
+                            backgroundColor: "transparent",
+                            borderBottom: "none",
+                        }}
+                    >
+                        {items.map((item) => (
+                            <Menu.Item
+                                key={item.key}
+                                style={{
+                                    maxWidth: "150px",
+                                }}
+                            >
+                                {item.label}
+                            </Menu.Item>
+                        ))}
+                    </Menu>
                 </Header>
-                <Content style={{ margin: "0 16px", minHeight: "100vh" }}>{children}</Content>
+                <Content style={{ margin: "0 16px", minHeight: "100vh" }}>
+                    {children}
+                </Content>
                 <Footer style={{ textAlign: "center" }}>
                     SkyRatings © 2024 Created by Arthur LORY, BERTIN Alexis,
                     YVENOU Pierre, HUCHEDE Simon
